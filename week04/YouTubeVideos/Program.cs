b855567e8e161f4827ed6b2b9bf83f65711c711b
Wisdoxl <wisdoxl@gmail.com>
2025-10-09 12:52:18 +0100

    Modified

--- week04/YouTubeVideos/Program.cs
+++ week04/YouTubeVideos/Program.cs
@@ -1,49 +1,103 @@
 using System;
 using System.Collections.Generic;
 
+class Comment
+{
+    private string _name;
+    private string _text;
+
+    public Comment(string name, string text)
+    {
+        _name = name;
+        _text = text;
+    }
+
+    public string GetName()
+    {
+        return _name;
+    }
+
+    public string GetText()
+    {
+        return _text;
+    }
+}
+
+class Video
+{
+    private string _title;
+    private string _author;
+    private int _length;
+    private List<Comment> _comments = new List<Comment>();
+
+    public Video(string title, string author, int length)
+    {
+        _title = title;
+        _author = author;
+        _length = length;
+    }
+
+    public void AddComment(Comment comment)
+    {
+        _comments.Add(comment);
+    }
+
+    public int GetNumberOfComments()
+    {
+        return _comments.Count;
+    }
+
+    public void DisplayInfo()
+    {
+        Console.WriteLine($"Title: {_title}");
+        Console.WriteLine($"Author: {_author}");
+        Console.WriteLine($"Length (seconds): {_length}");
+        Console.WriteLine($"Number of Comments: {GetNumberOfComments()}");
+        Console.WriteLine("Comments:");
+
+        foreach (Comment comment in _comments)
+        {
+            Console.WriteLine($" - {comment.GetName()}: {comment.GetText()}");
+        }
+
+        Console.WriteLine(new string('-', 40));
+    }
+}
+
 class Program
 {
     static void Main(string[] args)
     {
-        // Create a list to store all the videos
+        // Create list of videos
         List<Video> videos = new List<Video>();
 
-        // Create Video 1
-        Video video1 = new Video("How to Cook Jollof Rice", "Chef Tunde", 600);
-        video1.AddComment(new Comment("Amaka", "This looks delicious!"));
-        video1.AddComment(new Comment("John", "I tried this recipe and loved it."));
-        video1.AddComment(new Comment("Chika", "Can you make a version without meat?"));
+        // Create first video
+        Video video1 = new Video("How to Make Pancakes", "Chef Amaka", 300);
+        video1.AddComment(new Comment("John", "I tried this recipe and loved it!"));
+        video1.AddComment(new Comment("Grace", "So easy to follow, thanks!"));
+        video1.AddComment(new Comment("Liam", "Please make a chocolate version next."));
 
-        // Create Video 2
-        Video video2 = new Video("Learn C# in 10 Minutes", "CodeMaster", 900);
-        video2.AddComment(new Comment("Grace", "Very clear explanation!"));
-        video2.AddComment(new Comment("Sam", "This really helped me understand classes."));
-        video2.AddComment(new Comment("Ella", "Can you do one for LINQ next?"));
+        // Create second video
+        Video video2 = new Video("Learn C# in 10 Minutes", "CodeMaster", 600);
+        video2.AddComment(new Comment("Ella", "Super helpful for beginners."));
+        video2.AddComment(new Comment("Victor", "Please make one for LINQ!"));
+        video2.AddComment(new Comment("Tolu", "I finally understand classes."));
 
-        // Create Video 3
-        Video video3 = new Video("Top 10 Travel Destinations in Africa", "ExplorerTV", 720);
-        video3.AddComment(new Comment("Tomiwa", "I want to visit Kenya now!"));
-        video3.AddComment(new Comment("Michael", "Beautiful video editing."));
-        video3.AddComment(new Comment("Lola", "Please include Nigeria next time!"));
+        // Create third video
+        Video video3 = new Video("Top 5 Travel Destinations in Africa", "ExplorerTV", 720);
+        video3.AddComment(new Comment("Michael", "Kenya is definitely my favorite!"));
+        video3.AddComment(new Comment("Sarah", "Can you include Cape Verde next time?"));
+        video3.AddComment(new Comment("Chika", "Beautiful places!"));
 
-        // Add videos to list
+        // Add videos to the list
         videos.Add(video1);
         videos.Add(video2);
         videos.Add(video3);
 
-        // Display information for each video
-        foreach (Video v in videos)
+        // Display info for each video
+        foreach (Video video in videos)
         {
-            Console.WriteLine($"Title: {v.Title}");
-            Console.WriteLine($"Author: {v.Author}");
-            Console.WriteLine($"Length (seconds): {v.Length}");
-            Console.WriteLine($"Number of Comments: {v.GetCommentCount()}");
-            Console.WriteLine("Comments:");
-            foreach (Comment c in v.Comments)
-            {
-                Console.WriteLine($" - {c.Name}: {c.Text}");
-            }
-            Console.WriteLine(new string('-', 40));
+            video.DisplayInfo();
         }
     }
 }
